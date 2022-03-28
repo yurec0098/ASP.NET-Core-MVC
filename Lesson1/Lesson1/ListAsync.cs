bdf0d16dd18c75975a0283c271cc0f84ef1190d9
yurec0098 <yurec0098@rambler.ru>
2022-03-28 15:33:40 +0300

    4. Создайте класс-обертку над List<T>, что бы можно было добавлять и удалять элементы из разных потоков без ошибок

---
+++ Lesson1/Lesson1/ListAsync.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson1
+{
+	internal class ListAsync<T> : List<T> where T : class
+	{
+		private readonly object _lock = new object();
+		public new void Add(T item)
+		{
+			lock (_lock)
+				base.Add(item);
+		}
+		public new void AddRange(IEnumerable<T> collection)
+		{
+			lock (_lock)
+				base.AddRange(collection);
+		}
+		public new void Clear()
+		{
+			lock (_lock)
+				base.Clear();
+		}
+		public new bool Remove(T item)
+		{
+			lock (_lock)
+				return base.Remove(item);
+		}
+		public new int RemoveAll(Predicate<T> match)
+		{
+			lock (_lock)
+				return base.RemoveAll(match);
+		}
+		public new void RemoveAt(int index)
+		{
+			lock (_lock)
+				base.RemoveAt(index);
+		}
+		public new void RemoveRange(int index, int count)
+		{
+			lock (_lock)
+				base.RemoveRange(index, count);
+		}
+		public new void CopyTo(T[] array, int arrayIndex)
+		{
+			lock (_lock)
+				base.CopyTo(array, arrayIndex);
+		}
+		public new void CopyTo(T[] array)
+		{
+			lock (_lock)
+				base.CopyTo(array);
+		}
+		public new void CopyTo(int index, T[] array, int arrayIndex, int count)
+		{
+			lock (_lock)
+				base.CopyTo(index, array, arrayIndex, count);
+		}
+	}
+}
