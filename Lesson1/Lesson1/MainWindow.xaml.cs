b16bda390c73e44ab87a076128cbf9f102a5d5f0
yurec0098 <yurec0098@rambler.ru>
2022-03-28 09:00:16 +0300

    upd1. Создайте форму WPF или WinForms, разместите на ней текстовое поле и в другом потоке последовательно добавляйте на него числа Фибоначчи.

--- Lesson1/Lesson1/MainWindow.xaml.cs
+++ Lesson1/Lesson1/MainWindow.xaml.cs
@@ -12,6 +12,56 @@ namespace Lesson1_1
 		public MainWindow()
 		{
 			InitializeComponent();
+			DataContext = this;
+
+			new Thread(() => RunFibonachi(100, textBox)) { IsBackground = true }.Start();
+		}
+
+		private void RunFibonachi(int length, TextBox tb)
+		{
+			var fibonachiText = string.Empty;
+			tb.Dispatcher.Invoke(() => tb.Text = string.Empty);
+
+			for (int i = -100; i < length; i++)
+			{
+				if (string.IsNullOrEmpty(fibonachiText))
+					fibonachiText += FibonacciIteration(i);
+				else
+					fibonachiText += $", {FibonacciIteration(i)}";
+
+				tb.Dispatcher.Invoke(() => tb.Text = fibonachiText);
+				
+				Thread.Sleep(500);
+			}
+		}
+
+		private decimal FibonacciIteration(int n)
+		{
+			if (n == 0)
+				return 0;
+
+			bool isNegative = false;
+			if (n < 0)
+			{
+				n = -n;
+				isNegative = true;
+			}
+
+			decimal result = 1;
+			decimal f0 = 0;
+			decimal f1 = 1;
+			for (int i = 2; i <= n; i++)
+			{
+				result = f0 + f1;
+
+				f0 = f1;
+				f1 = result;
+			}
+
+			if (isNegative)
+				return n % 2 == 0 ? -result : result;
+			else
+				return result;
 		}
 	}
 }
