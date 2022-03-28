c6963c4276bd41db26f79357b697f87088e524cf
yurec0098 <yurec0098@rambler.ru>
2022-03-28 08:58:48 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Lesson1/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lesson1_1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
